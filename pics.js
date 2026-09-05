2f50274bce294a4cdf4b2081a5bd28476e119676
wallstreetbranchmanager-tech <wallstreetbranchmanager@gmail.com>
2026-09-05 08:54:19 -0400

    Load desk portrait from existing cass file until new chunk lands

--- pics.js
+++ pics.js
@@ -4,7 +4,7 @@ const PARTS = {
   home: ["home.b64"],
   flood: ["flood.b64"],
   sky: ["sky.b64"],
-  desk: ["desk.b64"]
+  desk: ["cass.b64"]
 };
 async function load(name) {
   const texts = await Promise.all(PARTS[name].map(function (f) {
